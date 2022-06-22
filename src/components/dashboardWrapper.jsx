73fb03375e3b506ca45212a6a8aabb07be3e8a8c
Franco Ciprian <franco_ciprian@hotmail.com>
2022-06-22 16:32:42 -0300

    Changing Tooltip

--- src/components/dashboardWrapper.jsx
+++ src/components/dashboardWrapper.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import ToogleDarkMode from './toogleDarkMode/ToogleDarkMode';
 
-import { Box, Text, Flex, IconButton, Tooltip, Container, useTheme } from '@chakra-ui/react'; // chakra-ui
+import { Box, Text, Flex, IconButton, Tooltip, Container, useTheme, useColorModeValue } from '@chakra-ui/react'; // chakra-ui
 import { FiLogOut } from "react-icons/fi"; //React-icons
 
 export default function DashboardWrapper({children}) {
-  // const bg = useColorModeValue("link.light", "link.dark");
-  // const text = useColorModeValue("text.light", "text.dark");
+  const bg = useColorModeValue("bg.light", "bg.dark");
+  const color = useColorModeValue("gray.900", "gray.100");
 
   const theme = useTheme()
   console.log(theme)
@@ -25,14 +25,14 @@ export default function DashboardWrapper({children}) {
         </Box>
         <Flex w='30%' justify='flex-end' align='center' gap='3'>
           <Box w='100%' display='flex' gap='10px' justifyContent='flex-end'>
-            <Tooltip label="Links" aria-label='Links'>
+            <Tooltip label="Links" aria-label='Links' bg={bg} color={color} borderWidth="1px">
               <Link to="/dashboard">Links</Link>
             </Tooltip>
-            <Tooltip label="Profile" aria-label='Profile'>
+            <Tooltip label="Profile" aria-label='Profile' bg={bg} color={color} borderWidth="1px">
               <Link to="/dashboard/profile">Profile</Link>
             </Tooltip>
           </Box>
-          <Tooltip label="Signout" aria-label='Signout'>
+          <Tooltip label="Signout" aria-label='Signout' bg={bg} color={color} borderWidth="1px">
             <Link to="/signout" >
               <IconButton 
                 size='md' 
--- src/components/toogleDarkMode/ToogleDarkMode.jsx
+++ src/components/toogleDarkMode/ToogleDarkMode.jsx
@@ -1,12 +1,15 @@
-import { Box, IconButton, useColorMode, Tooltip } from '@chakra-ui/react'
+import { Box, IconButton, useColorMode, Tooltip, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
 export default function ToogleDarkMode() {
   const {colorMode, toggleColorMode} = useColorMode()
 
+  const bg = useColorModeValue("bg.light", "bg.dark");
+  const color = useColorModeValue("gray.900", "gray.100");
+
   return (
     <Box>
-      <Tooltip label="Theme" aria-label='theme'>
+      <Tooltip label="Theme" aria-label='theme' bg={bg} color={color} borderWidth="1px">
         <IconButton 
             size='md' 
             icon={colorMode === 'light' ? <MoonIcon color='#2C5282'/> : <SunIcon color='#F6AD55' />  }
