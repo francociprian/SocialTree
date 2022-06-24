d7228a28cf8507820501823ea6034c906dfc0fdc
Franco Ciprian <franco_ciprian@hotmail.com>
2022-06-24 11:23:09 -0300

    Changes page choose username

--- src/routes/ChooseUsernameView.jsx
+++ src/routes/ChooseUsernameView.jsx
@@ -2,8 +2,9 @@ import AuthProvider from "../components/authProvider";
 import { useState } from "react";
 import { useNavigate, Link } from 'react-router-dom';
 import { existsUsername, updateUser } from "../firebase/firebase";
+import ToogleDarkMode from "../components/toogleDarkMode/ToogleDarkMode";
 
-import { Center ,Spinner } from "@chakra-ui/react"; //Chakra ui
+import {  Center , Container, Flex, Input, Spinner, Text, VStack, Stack, Button, FormControl } from "@chakra-ui/react"; //Chakra ui
 
 export default function ChooseUsernameView() {
   const navigate = useNavigate();
@@ -14,11 +15,14 @@ export default function ChooseUsernameView() {
 
   function handleUserLoggedIn() {navigate('/dashboard') }
   function handleUserNotRegistered(user) {
-    setState(3)
     setCurrentUser(user);
+    setState(3)
   }
   function handleUserNotLoggedIn() { navigate('/login') }
   function handleInputUsername(e) {
+    if(e.target.value.length < 0) {
+      console.log('error')
+    }
     setUsername(e.target.value);
   }
   async function handleContinue() {
@@ -35,32 +39,76 @@ export default function ChooseUsernameView() {
       }
     }
   }
-  
+
   if(state === 3 || state === 5) {
     return (
-      <div>
-        <h1>Welcome {currentUser.displayName}</h1> 
-        <p>Para Terminar el proceso elige un nombre de usuario</p>
-        {state === 5 ? <p>El nombre de usuario ya existe, escoge otro</p> : ''}
-        <div>
-          <input 
-            type="text"
-            onChange={handleInputUsername} 
-            placeholder="Nombre de usuario" />
-        </div>
-        <div>
-          <button onClick={handleContinue}>Continuar</button>
-        </div>
-      </div>
+      <Container maxW='container.xl'>
+        <Flex justify='flex-end' mt='4'>
+          <ToogleDarkMode />
+        </Flex>
+        <VStack h='85vh' w='full' justify='center' align='center' >
+          <>
+            <Stack 
+              direction={['column', 'column', 'row', 'row']}
+              align={['center', 'center', 'flex-start', 'flex-start']}
+              fontSize={['5xl', '5xl', '4.8vw', '4.8vw']}
+              my='10'
+              fontWeight='black'
+              letterSpacing='tight'
+              >
+              <Text as='h1' mr='4'>
+                Welcome 
+              </Text> 
+              <Text 
+                borderWidth='1px'
+                borderRadius='xl'
+                p='0.3rem 1.5rem'
+                bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
+                bgClip="text"
+              >{currentUser.displayName}</Text>
+            </Stack> 
+            <Container maxW='container.sm'>
+              <Text fontSize={['md', 'md', 'xl', 'xl']} >To finish the process choose a username</Text>
+              <FormControl display='flex' flexDirection='column' alignItems='center' gap='3'>
+                <Input
+                  type="text"
+                  mt='3'
+                  variant='filled'
+                  onChange={handleInputUsername} 
+                  placeholder="@username" 
+                />
+                {username === '' ? <Text fontFamily='mono' fontSize='sm' bg='rgba(254, 178, 178, 0.16)' color='tomato' px='3px'>Please enter a username</Text> : null}
+                {state === 5 ? <Text fontFamily='mono' fontSize='sm' bg='rgba(254, 178, 178, 0.16)' color='tomato' px='3px'>The username already exists, choose another!</Text> :   null }
+                <Button onClick={handleContinue}>Continue</Button>
+              </FormControl>
+            </Container>
+          </>
+        </VStack>
+      </Container>
     )
   }
 
   if(state === 6) {
     return (
-      <div>
-        <h1>Felicidades, ya puedes ir al dashboard a crear tus links</h1>
-        <Link to="/dashboard">Continuar</Link>
-      </div>
+      <Container maxW='container.xl'>
+        <Flex justify='flex-end' mt='4'>
+          <ToogleDarkMode />
+        </Flex>
+        <VStack h='80vh' justify='center' align='center' gap={5}>
+          <Text
+            fontSize='4.8vw' 
+            fontWeight='black'
+            letterSpacing='tight'
+            p='0.3rem 1.5rem'
+            bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
+            bgClip="text"
+          >Congratulations</Text>
+          <Text fontSize='xl' >Go to the dashboard!!</Text>
+          <Button>
+            <Link to="/dashboard">Continue</Link>
+          </Button>
+        </VStack>
+      </Container>
   )}
 
   return (
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,12 @@
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+const breakpoints = createBreakpoints({
+  sm: '40em',
+  md: '52em',
+  lg: '64em',
+  xl: '80em',
+});
 
 const theme = {
   config: {
@@ -46,6 +54,7 @@ const theme = {
       },
     },
   },
+  breakpoints,
   style: {
     global: {
       body: {
